be232a13793c97d119fcff720930b68d3e308672
Dmitri Iselund <Dmitri.iselund@gmail.com>
2022-01-09 15:47:48 +0100

    first c ommit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame, sys, time
+from settings import *
+from level import Level
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+level = Level(screen)
+timer = False
+delay = 0.05
+jump_bool = True
+
+def switchbool(jump_bool):
+    if jump_bool == True:
+        jump_bool = False
+    elif jump_bool == False:
+        jump_bool = True
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                x = time.time()
+                timer = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                timer = False
+                jump_bool = True
+    
+    if timer: 
+        if time.time() - x >= delay:
+            jump_bool = False
+    
+    screen.fill('black')
+    level.run(jump_bool)
+
+    pygame.display.update()
+    clock.tick(60)
+
---
+++ settings.py
@@ -0,0 +1,19 @@
+level_map = [
+'                   ',
+'                   ',
+'                   ', 
+'                   ', 
+'                   ', 
+'                   ',
+'                   ',     
+'                   ',
+'                   ',
+'                   ',
+'                   ',
+'    XXX            ', 
+'    P              ', 
+'   XXXXXX          ',   ]
+
+tile_size = 32
+screen_width = 500
+screen_height = 800
